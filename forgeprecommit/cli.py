d5a5d1e4eb550e16ed9e53d56a7b869d71a59f90
Dave Gaeddert <dave.gaeddert@gmail.com>
2023-01-19 17:25:24 -0600

    Fix

--- forgeprecommit/cli.py
+++ forgeprecommit/cli.py
@@ -18,15 +18,10 @@ def cli(ctx, install):
         install_git_hook()
         return
 
-    if forgepackage_installed("forge-format"):
+    if forgepackage_installed("format"):
         forge.venv_cmd("forge", "format", "--check", check=True)
 
-    if not django_db_connected():
-        click.echo()
-        click.secho("Skipping database checks", bold=True, fg="yellow")
-        click.secho("Running Django checks (without database)", bold=True)
-        forge.manage_cmd("check", check=True)
-    else:
+    if django_db_connected():
         click.echo()
         click.secho("Running Django checks", bold=True)
         forge.manage_cmd("check", "--database", "default", check=True)
@@ -35,11 +30,17 @@ def cli(ctx, install):
         click.secho("Checking Django migrations", bold=True)
         forge.manage_cmd("migrate", "--check", check=True)
 
-    click.echo()
-    click.secho("Checking for Django models missing migrations", bold=True)
-    forge.manage_cmd("makemigrations", "--dry-run", "--check", check=True)
+        click.echo()
+        click.secho("Checking for Django models missing migrations", bold=True)
+        forge.manage_cmd("makemigrations", "--dry-run", "--check", check=True)
+    else:
+        click.echo()
+        click.secho("Running Django checks (without database)", bold=True)
+        forge.manage_cmd("check", check=True)
 
-    if forgepackage_installed("forge-test"):
+        click.secho("Skipping migration checks", bold=True, fg="yellow")
+
+    if forgepackage_installed("test"):
         click.echo()
         click.secho("Running tests", bold=True)
         forge.venv_cmd("forge", "test", check=True)
